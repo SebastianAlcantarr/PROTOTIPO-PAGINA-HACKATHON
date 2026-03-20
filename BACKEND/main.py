3923a369e9b14b1034a0f1e28f1505fc97250fb5
Sebastian Alcantar VIcencio <sebastianalcantar323@gmail.com>
2026-03-19 17:33:43 -0700

    LOCAL

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -40,3 +40,8 @@ async def analizar(params: InversionParametros):
 @app.get("/")
 def root():
     return {"status": "ok", "mensaje": "API activa"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
